5007188a98c7ca305c69ef9f8c9ddea4bcb7c2ba
stanislavsabev <bezraboten.34@gmail.com>
2023-06-14 12:25:53 +0200

    Rename WordCounterTest file

---
+++ test/WordsCountTests/WordCounterTests.cs
@@ -0,0 +1,21 @@
+
+namespace WordsCountTests
+{
+
+    public class WordCounterTests
+    {
+        private readonly WordCounter wordCounter;
+
+        public WordCounterTests()
+        {
+            wordCounter = new WordCounter();
+        }
+
+        [Fact]
+        public void GetWordsCountTest()
+        {
+
+        }
+
+    }
+}
