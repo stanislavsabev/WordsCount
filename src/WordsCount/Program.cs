db6b3c41cce464766333ac89e1014e1c2e79ddf2
stanislavsabev <bezraboten.34@gmail.com>
2023-06-14 11:36:50 +0200

    Initial commit

---
+++ src/WordsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 1){
+            Console.Write("usage: WordsCount <file-path>");
+            return;
+        }
+
+        string filePath = args[0];
+        IEnumerable<string> lines = File.ReadLines(filePath);
+
+        WordCounter counter = new WordCounter();
+        Dictionary<string, int> wordCounts = counter.GetWordsCount(
+            lines, new char[] { '.', ',', ';', ':', '!', '?' });
+
+        foreach (string key in wordCounts.Keys)
+        {
+            Console.WriteLine(key + ": " + wordCounts[key]);
+        }
+    }
+}
---
+++ src/WordsCount/WordsCounter.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+class WordCounter
+{
+
+    public Dictionary<string, int> GetWordsCount(IEnumerable<string> lines, char[]? trimChars = null)
+    {
+        var wordCounts = new Dictionary<string, int>();
+
+        foreach (string line in lines)
+        {
+            string[] words = line.Split(' ');
+
+            foreach (string word in words)
+            {
+                string trimmedWord = word.Trim(trimChars).ToLower();
+                if (wordCounts.ContainsKey(trimmedWord))
+                {
+                    wordCounts[trimmedWord]++;
+                }
+                else
+                {
+                    wordCounts.Add(trimmedWord, 1);
+                }
+            }
+
+        }
+        return wordCounts;
+    }
+}
+
